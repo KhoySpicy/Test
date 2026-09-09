ac46fa3b6cb87b6ce3958eae62a6fb50f10174fd
KhoySpicy <huynhkhoi.2007kg@gmail.com>
2026-09-09 19:20:48 +0700

    Update DialogueData.js

--- DialogueData.js
+++ DialogueData.js
@@ -92,7 +92,7 @@ const dialogueData =[
 {
     id: "greetend",
   Name: "Monika",
-  avatar:"Mon5.wepb",
+  avatar:"Mon5.webb",
   text:"Tạm thời như thế đi. Công việc của tôi ở đây cũng xong rồi, chúng ta sẽ gặp lại sau ",
       
 },
